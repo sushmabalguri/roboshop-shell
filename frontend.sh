2743184bffab71d5617bfbdd84970bbb3debc46d
sushma <sushmabalguri6677@gmail.com>
2023-08-28 14:16:32 -0700

    roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -3,8 +3,8 @@ cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl enable nginx
-systemctl start nginx
-systemctl restart nginx
+#cd /usr/share/nginx/html
+#unzip /tmp/frontend.zip
+
+#systemctl enable nginx
+#systemctl restart nginx
